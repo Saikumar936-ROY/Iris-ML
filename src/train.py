f80adf1adcaf59f5a4857df18c022d794a74d2b9
Saikumar936-ROY <lingarajusaikumariit@gmail.com>
2025-07-09 12:02:15 +0530

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,1 +1,16 @@
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+import joblib
 
+# Load data
+df = pd.read_csv('data/Iris.csv')
+X = df.drop('Species', axis=1)
+y = df['Species']
+
+# Train model
+model = KNeighborsClassifier()
+model.fit(X, y)
+
+# Save model
+joblib.dump(model, 'model/model.pkl')
+print(" Iris flower model trained.")
