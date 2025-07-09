0e173aa08233fb0c9d43fff9f279cf8a74c9ac2c
Saikumar936-ROY <lingarajusaikumariit@gmail.com>
2025-07-09 12:09:10 +0530

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -1,1 +1,18 @@
+import pandas as pd
+import joblib
+
+# Load trained model
+model = joblib.load('model/model.pkl')
+
+# Sample flower data
+sample = pd.DataFrame({
+    'SepalLengthCm': [5.1],
+    'SepalWidthCm': [3.5],
+    'PetalLengthCm': [1.4],
+    'PetalWidthCm': [0.2]
+})
+
+# Predict species
+prediction = model.predict(sample)[0]
+print(f"🌸 Predicted Iris Species: {prediction}")
 
